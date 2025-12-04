9531d0189b0a9688bfdd43ab837bdd40b74d14e1
applumbrado <alumbrado_app@centro.gob.mx>
2025-12-04 00:56:42 +0000

    alumun - A0_k | LVIV 12.38 Production

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,11 +18,38 @@ import io from 'socket.io-client'
 
 window.io = io
 
+// window.Echo = new Echo({
+//     broadcaster: 'socket.io',
+//     host: window.location.hostname + ':6001', // http://localhost:6001
+//     transports: ['polling','websocket'],
+// })
+
+const isLocal = window.location.hostname === 'localhost';
+const echoHost = isLocal ? 'http://localhost:6001' : 'https://alumbrado.villahermosa.gob.mx:6001';
+
+
 window.Echo = new Echo({
     broadcaster: 'socket.io',
-    host: window.location.hostname + ':6001', // http://localhost:6001
-    transports: ['polling','websocket'],
-})
+    host: window.location.hostname === 'localhost'
+        ? 'http://localhost:6001'
+        : 'https://alumbrado.villahermosa.gob.mx:6001',
+    transports: ['polling', 'websocket'],  // usa ambos, polling primero
+    forceNew: true,
+    reconnectionAttempts: 10,
+    reconnectionDelay: 20000,
+    timeout: 100000,
+    secure: true,
+    forceTLS: true,
+    rejectUnauthorized: false,
+    auth: {
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token') // si usas tokens
+        }
+    },
+
+});
+
+
 
 // Logs para debug
 if (window.Echo.connector && window.Echo.connector.socket) {
