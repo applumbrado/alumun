026f8226a6ac6f79467c609c2552774db7b01a4c
applumbrado <alumbrado_app@centro.gob.mx>
2025-12-20 13:24:14 +0000

    alumun - A0_s | LVIV 12.38 Production

--- app/Services/CFE/CFEImportService.php
+++ app/Services/CFE/CFEImportService.php
@@ -194,7 +194,7 @@ class CFEImportService{
                 $medidor = (string)($cls->NUMMED1 ?? '');
                 $tarifa  = (string)($cls->TARIFA ?? '');
 
-                dd($cuenta);
+//                dd($cuenta);
 
                 // PeriodoStr
                 $desdeRaw   = (string)($cls->FECDESDE ?? '');
